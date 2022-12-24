c656fcd4f66ace6a7edce4fd49a3dff187efae25
Finlay Marno <finlay.marno@codeplay.com>
2022-12-24 00:15:26 +0000

    still working on thirteen

--- src/thirteen.zig
+++ src/thirteen.zig
@@ -3,6 +3,7 @@ const utils = @import("utils.zig");
 const parseUnsigned = std.fmt.parseUnsigned;
 const print = std.debug.print;
 const isDigit = std.ascii.isDigit;
+const Order = std.math.Order;
 
 const ItemTag = enum {
     int,
@@ -26,32 +27,48 @@ fn getType(line: []const u8) ItemTag {
     return .int;
 }
 
-//const ReadInt = struct {
-//    val: u32,
-//    the_rest: []u8,
-//};
 fn readInt(line: []const u8) u32 {
-    println("readInt", line);
-    //const end = std.mem.indexOfAny(u8, line, ",]").?;
     return std.fmt.parseUnsigned(u32, line, 10) catch unreachable;
-    //return .{ .val = val, .the_rest = line[end + 1 ..] };
 }
+
 const ItemIter = struct {
     line: []const u8,
     fn next(self: *ItemIter) ?[]const u8 {
         if (self.line.len == 0) return null;
-        const end = std.mem.indexOfAny(u8, self.line, ",]") orelse self.line.len;
-        var ret = self.line[0..end];
-        print("l:{s} ret:{s} end:{}\n", .{ self.line, ret, end });
-        self.line = self.line[end + 1 ..];
-        return ret;
+        const ty = getType(self.line);
+        if (ty == .int) {
+            const end = std.mem.indexOfAny(u8, self.line, ",]");
+            if (end) |e_idx| {
+                const ret = self.line[0..e_idx];
+                self.line = self.line[e_idx + 1 ..];
+                return ret;
+            } else {
+                const ret = self.line;
+                self.line = &.{};
+                return ret;
+            }
+        } else {
+            var open: u32 = 1;
+            var close: u32 = 0;
+            var idx: usize = 1;
+            while (open != close) : (idx += 1) {
+                if (self.line[idx] == '[') {
+                    open += 1;
+                } else if (self.line[idx] == ']') {
+                    close += 1;
+                }
+            }
+            const ret = self.line[0..idx];
+            self.line = if (idx == self.line.len) &.{} else self.line[idx + 1 ..];
+            return ret;
+        }
     }
 };
 
-fn compareList(lhs: []const u8, rhs: []const u8) std.math.Order {
+fn compareList(lhs: []const u8, rhs: []const u8) Order {
     // skip opening [
-    var lhs_iter = ItemIter{ .line = lhs[1..] };
-    var rhs_iter = ItemIter{ .line = rhs[1..] };
+    var lhs_iter = ItemIter{ .line = lhs[1 .. lhs.len - 1] };
+    var rhs_iter = ItemIter{ .line = rhs[1 .. rhs.len - 1] };
     while (true) {
         //skip the opening [
         const lhs_item = lhs_iter.next();
@@ -74,36 +91,40 @@ fn compareList(lhs: []const u8, rhs: []const u8) std.math.Order {
     }
 }
 
-fn compareIntList(item: []const u8, list: []const u8) std.math.Order {
-    const items = std.mem.count(u8, list, ",") + 1;
-    if (items > 1) return .gt;
-    const num = std.mem.indexOfAny(u8, list, "0123456789") orelse return .lt;
-    const list_val = readInt(list[num..]);
-    const val = readInt(item);
+fn compareIntList(item: []const u8, list: []const u8) Order {
+    print("CompareIntList {s} vs {s}\n", .{ item, list });
+    var iter = ItemIter{ .line = list[1 .. list.len - 1] };
+    const demote = iter.next() orelse return .gt;
+    return compareItem(item, demote);
 
-    return std.math.order(val, list_val);
 }
 
-fn compareItem(lhs: []const u8, rhs: []const u8) std.math.Order {
+fn compareItem(lhs: []const u8, rhs: []const u8) Order {
+    print("Compare {s} vs {s}\n", .{ lhs, rhs });
     const lhs_type = getType(lhs);
     const rhs_type = getType(rhs);
 
-    if (lhs_type == rhs_type) {
-        if (lhs_type == .int) {
-            const lhs_value = readInt(lhs);
-            const rhs_value = readInt(rhs);
-            return std.math.order(lhs_value, rhs_value);
+    const result = blk: {
+        if (lhs_type == rhs_type) {
+            if (lhs_type == .int) {
+                const lhs_value = readInt(lhs);
+                const rhs_value = readInt(rhs);
+                break :blk std.math.order(lhs_value, rhs_value);
+            } else {
+                break :blk compareList(lhs, rhs);
+            }
         } else {
-            return compareList(lhs, rhs);
+            if (lhs_type == .int) return compareIntList(lhs, rhs);
+            const r: Order = switch (compareIntList(rhs, lhs)) {
+                .lt => .gt,
+                .gt => .lt,
+                .eq => .eq,
+            };
+            break :blk r;
         }
-    } else {
-        if (lhs_type == .int) return compareIntList(lhs, rhs);
-        return switch (compareIntList(rhs, lhs)) {
-            .lt => .gt,
-            .gt => .lt,
-            .eq => .eq,
-        };
-    }
+    };
+    print("compare {s} and {s} = {}\n", .{ lhs, rhs, result });
+    return result;
 }
 
 pub fn main() !void {
@@ -111,7 +132,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var all = try utils.readAll(allocator, "input/test");
+    var all = try utils.readAll(allocator, "input/thirteen");
     defer allocator.free(all);
 
     var lines = std.mem.tokenize(u8, all, "\n");
@@ -120,8 +141,11 @@ pub fn main() !void {
     while (true) : (idx += 1) {
         const lhs = lines.next() orelse break;
         const rhs = lines.next().?;
-        if (compareItem(lhs, rhs) != .gt) {
-            sum += idx;
+        print("\n", .{});
+        switch (compareItem(lhs, rhs)) {
+            .lt => sum += idx + 1,
+            .gt => {},
+            .eq => unreachable,
         }
     }
 
